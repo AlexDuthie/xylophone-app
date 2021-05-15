4781d9c5c78fb804819ca43079f4f5103dca6bed
ALEX DUTHIE <alexduthiemain@outlook.com>
2021-05-16 00:46:13 +0100

    AudioPlayers dependency added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -11,7 +11,12 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(child: TextButton(
+            onPressed: (){
+
+            },
+            child: Text('Click Me'),
+          )),
         ),
       ),
     );
