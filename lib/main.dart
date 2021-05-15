915cb1b8222f2807b4c0c94fef11b586f8580747
ALEX DUTHIE <alexduthiemain@outlook.com>
2021-05-15 23:36:54 +0100

    Material App Structure Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() => runApp(XylophoneApp());
 
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
 }
