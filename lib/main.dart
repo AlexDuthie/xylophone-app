58544697f6363d669be77eee36de5fc3f09c0a01
ALEX DUTHIE <alexduthiemain@outlook.com>
2021-05-17 18:38:20 +0100

    Button that loops keys to replicate twinkle little star

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,25 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$note.wav');
   }
 
-  Expanded buildKey(Color color, int note) {
+  void playSong() async {
+    List<int> notes = [
+      1, 1, 5, 5, 6, 6, 5, 4, 4, 3, 3, 2, 2, 1,
+      5, 5, 4, 3, 3, 2, 5, 5, 4, 3, 3, 2,
+      1, 1, 5, 5, 6, 6, 5, 4, 4, 3, 3, 2, 2, 1
+    ];
+    // notes.add(1);
+    // notes.add(2);
+    // notes.add(3);
+
+    for (int i = 0; i < notes.length; i++) {
+      playSound(notes[i]);
+      await Future.delayed(Duration(milliseconds: 500));
+    }
+  }
+
+  Expanded buildKey(String key, Color color, int note) {
     return Expanded(
+      key: Key(key),
       child: TextButton(
         child: null,
         style: TextButton.styleFrom(
@@ -47,6 +64,21 @@ class XylophoneApp extends StatelessWidget {
                   ),
                   Icon(Icons.music_note),
                 ],
+              ),
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: (){
+                      playSong();
+                    },
+                    child: Text(
+                      'Click Me',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
               )
             ],
           ),
@@ -56,13 +88,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              buildKey(Colors.red, 1),
-              buildKey(Colors.blue, 2),
-              buildKey(Colors.yellow, 3),
-              buildKey(Colors.green, 4),
-              buildKey(Colors.teal, 5),
-              buildKey(Colors.purple, 6),
-              buildKey(Colors.orange, 7),
+              buildKey("1", Colors.red, 1),
+              buildKey("2", Colors.blue, 2),
+              buildKey("3", Colors.yellow, 3),
+              buildKey("4", Colors.green, 4),
+              buildKey("5", Colors.teal, 5),
+              buildKey("6", Colors.purple, 6),
+              buildKey("7", Colors.orange, 7),
             ],
           ),
         ),
